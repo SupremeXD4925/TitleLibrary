8fca997f584a5c5a3db70b6c7630c361daed9555
SupremeXD4925 <supreme.xd4625@gmail.com>
2024-11-17 18:43:13 +0100

    Added .github

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     description="A simple Python library to change the terminal window title",  # Short description
     author="SupremeXD",  # Your name (can be your real name or handle)
     author_email="neon321ontg@gmail.com",  # Your email
-    url="https://github.com/SupremeXD4925",  # URL to the library (GitHub repo, for example)
+    url="https://github.com/SupremeXD4925/TitleLibrary",  # URL to the library (GitHub repo, for example)
     long_description=open('README.md').read(),  # A long description, usually from README.md
     long_description_content_type="text/markdown",  # Content type for the README
     license="MIT",  # License for your code
